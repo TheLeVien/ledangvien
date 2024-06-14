4dae3f73bcbc9f1781d682af299a336fd600b470
TheVien <vienngao9@gmail.com>
2024-06-14 19:50:32 +0700

    ledangvien

--- game.js
+++ game.js
@@ -7,6 +7,15 @@ function back() {
   window.location.href = "main.html";
 }
 
+function pics0() {
+  event.preventDefault();
+  let pic1 = document.getElementById("pics0").src;
+  let picbig = document.getElementById("picsbig").src;
+  if (pic1 == picbig) return;
+  document.getElementById("pics0").src = picbig;
+  document.getElementById("picsbig").src = pic1;
+}
+
 function pics1() {
   event.preventDefault();
   let pic1 = document.getElementById("pics1").src;
@@ -34,11 +43,42 @@ function pics3() {
   document.getElementById("picsbig").src = pic1;
 }
 
-function pics4() {
-  event.preventDefault();
-  let pic1 = document.getElementById("pics4").src;
-  let picbig = document.getElementById("picsbig").src;
-  if (pic1 == picbig) return;
-  document.getElementById("pics4").src = picbig;
-  document.getElementById("picsbig").src = pic1;
+const urlgame =
+  "https://free-to-play-games-database.p.rapidapi.com/api/game?id=";
+
+async function getgame() {
+  let id = localStorage.getItem("id");
+  const options = {
+    method: "GET",
+    headers: {
+      "x-rapidapi-key": "74fb14a44fmsh73d1f679c292b73p12f2aejsn28969c2b5652",
+      "x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com",
+    },
+  };
+  try {
+    const response = await fetch(urlgame + id, options);
+    const result = await response.json();
+    console.log(result);
+    document.getElementById("picsbig").src = result.thumbnail;
+
+    document.getElementById("name").innerHTML = result.title;
+    document.getElementById("tit").innerHTML = result.title;
+    document.getElementById("des").innerHTML = result.description;
+    document.getElementById("date").innerHTML = result.release_date;
+    document.getElementById("genre").innerHTML = result.genre;
+
+    for (let i in result.screenshots) {
+      document.getElementById("pics" + i).src = result.screenshots[i].image;
+      console.log(document.getElementById("pics" + i).src);
+    }
+    ///console.log(document.getElementById("pics3").src);
+    if (document.getElementById("pics3").src == "") {
+      let element = document.getElementById("pics3");
+      element.remove();
+    }
+  } catch (error) {
+    console.error(error);
+  }
 }
+
+getgame();
--- main.js
+++ main.js
@@ -1,15 +1,58 @@
+localStorage.clear();
+let gameslot = document.getElementById("gameslot");
+const urllist = "https://free-to-play-games-database.p.rapidapi.com/api/games";
+const urlgame =
+  "https://free-to-play-games-database.p.rapidapi.com/api/game?id=";
+
+async function getlist() {
+  const options = {
+    method: "GET",
+    headers: {
+      "x-rapidapi-key": "74fb14a44fmsh73d1f679c292b73p12f2aejsn28969c2b5652",
+      "x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com",
+    },
+  };
+  try {
+    const response = await fetch(urllist, options);
+    const result = await response.json();
+    ///console.log(result);
+    for (let i = 0; i <= 20; i++) {
+      ///console.log(result[i].id);
+
+      /// gameslot
+      const game = document.createElement("div");
+      game.classList.add("game");
+      gameslot.appendChild(game);
+
+      /// pics
+      const pics = document.createElement("img");
+      pics.classList.add("pics");
+      pics.setAttribute("src", result[i].thumbnail);
+      game.appendChild(pics);
+
+      /// name
+      const name = document.createElement("div");
+      name.classList.add("name");
+      name.innerHTML = result[i].title;
+      name.id = result[i].id;
+      pics.id = result[i].id;
+      name.onclick = getgame;
+      pics.onclick = getgame;
+      game.appendChild(name);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function getgame() {
+  localStorage.setItem("id", event.target.id);
+  window.location = "game.html";
+}
+
+getlist();
+
 function loadGOW() {
-  window.location = "GOW.html";
-}
-
-function loadDoom() {
-  window.location = "Doom.html";
-}
-
-function loadBMW() {
-  window.location = "BMW.html";
-}
-
-function loadARK() {
-  window.location = "ARK.html";
+  ///event.preventDefault();
+  window.location = "game.html";
 }
